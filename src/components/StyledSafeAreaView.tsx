5532645b2a26a3f7d796ce3971cf4e0440b596e6
João Pedro <joaopedro.nc@outlook.com>
2023-11-02 20:39:58 -0300

    ✨ feat (ui): add StyledSafeAreaView component

---
+++ src/components/StyledSafeAreaView.tsx
@@ -0,0 +1,8 @@
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { styled } from 'tamagui'
+
+export const StyledSafeAreaView = styled(SafeAreaView, {
+  name: 'StyledSafeAreaView',
+  flex: 1,
+  backgroundColor: '$blue50',
+})
