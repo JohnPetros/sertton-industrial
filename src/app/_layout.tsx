5d50640fe725757ebd0274e52a33aa75869310f3
João Pedro <joaopedro.nc@outlook.com>
2023-11-02 20:32:41 -0300

    🗃️ file: delete useless files

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,52 +1,45 @@
-import { Suspense, useEffect } from "react";
-import { useColorScheme } from "react-native";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { SplashScreen, Stack } from "expo-router";
-import { TamaguiProvider, Text, Theme } from "tamagui";
+import { Suspense, useEffect } from 'react'
+import { useColorScheme } from 'react-native'
+import { StatusBar } from 'react-native'
+import { useFonts } from 'expo-font'
+import { SplashScreen, Stack } from 'expo-router'
+import { TamaguiProvider, Text, Theme } from 'tamagui'
 
-import { MySafeAreaView } from "../../components/MySafeAreaView";
-import config from "../../tamagui.config";
+import config from '../../tamagui.config'
+import { StyledSafeAreaView } from '../components/StyledSafeAreaView'
 
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function Layout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme()
 
   const [loaded] = useFonts({
-    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
-    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf")
-  });
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  })
 
   useEffect(() => {
     if (loaded) {
-      SplashScreen.hideAsync();
+      SplashScreen.hideAsync()
     }
-  }, [loaded]);
+  }, [loaded])
 
-  if (!loaded) return null;
+  if (!loaded) return null
 
   return (
     <TamaguiProvider config={config}>
       <Suspense fallback={<Text>Loading...</Text>}>
         <Theme name={colorScheme}>
-          <ThemeProvider
-            value={colorScheme === "light" ? DefaultTheme : DarkTheme}
-          >
-            <MySafeAreaView>
-              <Stack
-                screenOptions={{
-                  headerShown: false
-                }}
-              />
-            </MySafeAreaView>
-          </ThemeProvider>
+          <StatusBar backgroundColor="transparent" />
+          <StyledSafeAreaView>
+            <Stack
+              screenOptions={{
+                headerShown: false,
+              }}
+            />
+          </StyledSafeAreaView>
         </Theme>
       </Suspense>
     </TamaguiProvider>
-  );
+  )
 }
