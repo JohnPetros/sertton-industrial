f6844c92d333205fda3d552ed96054948e0b803b
João Pedro <joaopedro.nc@outlook.com>
2023-11-04 16:52:59 -0300

    ✨ feat: show collections on home screen

--- src/app/(drawer)/(tabs)/home.tsx
+++ src/app/(drawer)/(tabs)/home.tsx
@@ -1,9 +1,27 @@
-import { Text, YStack } from 'tamagui'
+import { YStack } from 'tamagui'
+
+import { Collection } from '@/components/Collection'
+import { Header } from '@/components/Header'
+import { Loading } from '@/components/Loading'
+import { Search } from '@/components/Search'
+import { useCollections } from '@/hooks/useCollections'
 
 export default function Home() {
+  const { collections, isLoading } = useCollections()
+
   return (
-    <YStack>
-      <Text>Home</Text>
+    <YStack px={24}>
+      <Header />
+      <Search />
+      <YStack mt={24}>
+        {isLoading ? (
+          <Loading message="Carregando coleção" />
+        ) : (
+          collections?.map((collection) => (
+            <Collection key={collection.id} data={collection} />
+          ))
+        )}
+      </YStack>
     </YStack>
   )
 }
