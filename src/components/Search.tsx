0367fab2d8cb02368cd4bb9fcb926f4008389e79
João Pedro <joaopedro.nc@outlook.com>
2023-11-03 22:17:52 -0300

    ✨ feat (ui): add Search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,31 @@
+import { Dimensions } from 'react-native'
+import { MagnifyingGlass } from 'phosphor-react-native'
+import { getTokens, XStack } from 'tamagui'
+
+import { Button } from '@/components/Button'
+import { Input } from '@/components/input'
+
+const SCREEN_WIDTH = Dimensions.get('screen').width
+const BUTTON_WIDTH = 48
+const X_PADDING_WIDTH = 24
+const GAP = 8
+const INPUT_WIDTH = SCREEN_WIDTH - BUTTON_WIDTH - GAP - X_PADDING_WIDTH * 2
+
+interface SearchProps {}
+
+export function Search({}: SearchProps) {
+  
+
+  return (
+    <XStack gap={GAP}>
+      <Input
+        placeholder="Exemplo: Arremate"
+        w={INPUT_WIDTH}
+        label="Procurar produto"
+      />
+      <Button w={BUTTON_WIDTH} background="primary" alignSelf="flex-end">
+        <MagnifyingGlass color={getTokens().color.white.val} />
+      </Button>
+    </XStack>
+  )
+}
