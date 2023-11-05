8574f4b3a7e6a63908749bb85cb57d2806d2dbab
João Pedro <joaopedro.nc@outlook.com>
2023-11-05 18:20:28 -0300

    ✨ feat (ux): cach api result data

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,10 @@
 import { Suspense, useEffect } from 'react'
 import { useColorScheme } from 'react-native'
 import { StatusBar } from 'react-native'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { useFonts } from 'expo-font'
 import { SplashScreen, Stack } from 'expo-router'
-import { getTokens, TamaguiProvider, Text, Theme, useTheme } from 'tamagui'
+import { TamaguiProvider, Text, Theme } from 'tamagui'
 
 import config from '../../tamagui.config'
 import { StyledSafeAreaView } from '../components/StyledSafeAreaView'
@@ -26,24 +27,28 @@ export default function Layout() {
 
   if (!loaded) return null
 
+  const queryClient = new QueryClient()
+
   return (
-    <TamaguiProvider config={config}>
-      <Suspense fallback={<Text>Loading...</Text>}>
-        <Theme name={colorScheme}>
-          <StatusBar
-            backgroundColor={'#f5f1f1'}
-            translucent
-            barStyle="dark-content"
-          />
-          <StyledSafeAreaView>
-            <Stack
-              screenOptions={{
-                headerShown: false,
-              }}
+    <QueryClientProvider client={queryClient}>
+      <TamaguiProvider config={config}>
+        <Suspense fallback={<Text>Loading...</Text>}>
+          <Theme name={colorScheme}>
+            <StatusBar
+              backgroundColor={'#f5f1f1'}
+              translucent
+              barStyle="dark-content"
             />
-          </StyledSafeAreaView>
-        </Theme>
-      </Suspense>
-    </TamaguiProvider>
+            <StyledSafeAreaView>
+              <Stack
+                screenOptions={{
+                  headerShown: false,
+                }}
+              />
+            </StyledSafeAreaView>
+          </Theme>
+        </Suspense>
+      </TamaguiProvider>
+    </QueryClientProvider>
   )
 }
