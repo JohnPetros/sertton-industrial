9efa48df2521bea3d359f2df77b395637ceabfb3
João Pedro <joaopedro.nc@outlook.com>
2023-11-05 18:16:56 -0300

    🧪 test: add skus mock

---
+++ src/tests/mocks/skusMock.ts
@@ -0,0 +1,34 @@
+import { Sku } from '@/@types/sku'
+
+export const skusMock: Sku[] = [
+  {
+    sku: 1001,
+    price_cost: 10.5,
+    price_sale: 15.0,
+    price_discount: 4.5,
+    weight: 0.5,
+    height: 5.0,
+    width: 2.5,
+    length: 8.0,
+  },
+  {
+    sku: 1002,
+    price_cost: 8.0,
+    price_sale: 12.0,
+    price_discount: 4.0,
+    weight: 0.4,
+    height: 4.0,
+    width: 3.0,
+    length: 7.0,
+  },
+  {
+    sku: 1003,
+    price_cost: 12.0,
+    price_sale: 18.0,
+    price_discount: 6.0,
+    weight: 0.8,
+    height: 6.0,
+    width: 3.5,
+    length: 10.0,
+  },
+]
