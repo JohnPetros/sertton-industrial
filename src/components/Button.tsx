281d40e9922ae549b6326f7292d91181e0f83ef5
João Pedro <joaopedro.nc@outlook.com>
2023-11-06 19:18:41 -0300

    💄 style: add transparent variant to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ export const Button = styled(TamaguiButton, {
   borderRadius: 4,
   borderWidth: 1,
 
-  focusStyle: {
+  pressStyle: {
     opacity: 0.7,
   },
 
@@ -25,6 +25,10 @@ export const Button = styled(TamaguiButton, {
         bg: '$colorTransparent',
         borderColor: '$blue400',
         color: '$blue400',
+      },
+      transparent: {
+        bg: '$colorTransparent',
+        color: '$gray800',
       },
     },
   } as const,
