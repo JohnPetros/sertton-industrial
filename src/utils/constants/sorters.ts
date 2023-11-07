106a922b99f42c3c2c71468aa83f22f80f3f8168
João Pedro <joaopedro.nc@outlook.com>
2023-11-07 18:27:13 -0300

    🔧 chore: add SORTERS constant

---
+++ src/utils/constants/sorters.ts
@@ -0,0 +1,14 @@
+import { Sorter } from '@/@types/order'
+
+export const SORTERS: Sorter[] = [
+  {
+    type: 'name',
+    name: 'A-Z',
+    order: 'asc',
+  },
+  {
+    type: 'name',
+    name: 'Z-A',
+    order: 'desc',
+  },
+]
