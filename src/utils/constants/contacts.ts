29023e0d9bb29eee2934b34887565453cfae4e53
João Pedro <joaopedro.nc@outlook.com>
2023-11-07 22:59:24 -0300

    🔧 chore: add CONTACTS constant

---
+++ src/utils/constants/contacts.ts
@@ -0,0 +1,12 @@
+import { Contact } from '@/@types/contact'
+
+export const CONTACTS: Contact[] = [
+  {
+    type: 'whatsapp',
+    value: '12 988234818',
+  },
+  {
+    type: 'landline',
+    value: '11 49682964',
+  },
+]
