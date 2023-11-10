8b9b452b845ad3f5e45472e68b70b54d8b26ad4a
João Pedro <joaopedro.nc@outlook.com>
2023-11-10 18:00:56 -0300

    🧪 test:  add cartItemsMock

---
+++ src/tests/mocks/cartItemsMock.ts
@@ -0,0 +1,32 @@
+import { Product } from '@/@types/product'
+import { productsMock } from '@/tests/mocks/productsMock'
+
+type CartItem = Product & { quantity: number; selectedSkuId: number }
+
+export const cartItemsMock: CartItem[] = [
+  {
+    ...productsMock[0],
+    quantity: 1,
+    selectedSkuId: 111,
+  },
+  {
+    ...productsMock[1],
+    quantity: 2,
+    selectedSkuId: 222,
+  },
+  {
+    ...productsMock[2],
+    quantity: 3,
+    selectedSkuId: 333,
+  },
+  {
+    ...productsMock[3],
+    quantity: 4,
+    selectedSkuId: 444,
+  },
+  {
+    ...productsMock[4],
+    quantity: 5,
+    selectedSkuId: 555,
+  },
+]
