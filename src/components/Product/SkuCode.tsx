a745382a54efb4181fabd18a47d2f6db5900e3a1
João Pedro <joaopedro.nc@outlook.com>
2023-11-10 16:32:21 -0300

    ✨ feat (ui): add SkuCode component

--- src/components/Product/SkuCode.tsx
+++ src/components/Product/SkuCode.tsx
@@ -1,4 +1,4 @@
-import { Text, TextProps } from 'tamagui'
+import { Text, TextProps, XStack } from 'tamagui'
 
 interface SkuCodeProps extends TextProps {
   children: number
@@ -6,8 +6,10 @@ interface SkuCodeProps extends TextProps {
 
 export function SkuCode({ children, ...rest }: SkuCodeProps) {
   return (
-    <Text fontSize={12} {...rest} fontWeight="600" color="$blue500">
-      {children}
-    </Text>
+    <XStack>
+      <Text fontSize={12} {...rest} fontWeight="600" color="$blue500">
+        SKU: {children}
+      </Text>
+    </XStack>
   )
 }
