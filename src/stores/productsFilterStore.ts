26c210f4efbcaaf916efbd7ccc29ac9b0142462f
João Pedro <joaopedro.nc@outlook.com>
2023-11-10 08:23:48 -0300

    ✨ feat (stores): add ProductsfilterStore

---
+++ src/stores/productsFilterStore.ts
@@ -0,0 +1,50 @@
+import { create } from 'zustand'
+import { immer } from 'zustand/middleware/immer'
+
+export type ProductsfilterStoreState = {
+  search: string
+  categoryId: number
+  brandsIds: number[]
+}
+
+type ProductsfilterStoreActions = {
+  setSearch: (search: string) => void
+  setCateforyId: (categoryId: number) => void
+  setBrandsIds: (brandsIds: number[]) => void
+}
+
+type ProductsfilterStoreProps = {
+  state: ProductsfilterStoreState
+  actions: ProductsfilterStoreActions
+}
+
+const initialState: ProductsfilterStoreState = {
+  search: '',
+  categoryId: 0,
+  brandsIds: [],
+}
+
+export const useProductsFilterStore = create<ProductsfilterStoreProps>()(
+  immer((set) => {
+    return {
+      state: initialState,
+      actions: {
+        setSearch(search: string) {
+          return set(({ state }) => {
+            state.search = search
+          })
+        },
+        setCateforyId(categoryId: number) {
+          return set(({ state }) => {
+            state.categoryId = categoryId
+          })
+        },
+        setBrandsIds(brandsIds: number[]) {
+          return set(({ state }) => {
+            state.brandsIds = brandsIds
+          })
+        },
+      },
+    }
+  })
+)
