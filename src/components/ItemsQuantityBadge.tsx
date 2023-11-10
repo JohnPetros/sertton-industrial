6dc35ada761db90748c2f6d9967c4e3cbc33fffc
João Pedro <joaopedro.nc@outlook.com>
2023-11-10 18:30:16 -0300

    ✨ feat (ui): add ItemsQuantityBadge component

---
+++ src/components/ItemsQuantityBadge.tsx
@@ -0,0 +1,29 @@
+import { Text, View } from 'tamagui'
+
+interface ItemsQuantityBadgeProps {
+  quantity: number
+  isActive: boolean
+}
+
+export function ItemsQuantityBadge({
+  quantity,
+  isActive,
+}: ItemsQuantityBadgeProps) {
+  return (
+    <View
+      position="absolute"
+      bg={isActive ? '$white' : '$blue400'}
+      w={24}
+      h={24}
+      borderRadius={12}
+      top={-8}
+      right={-8}
+      zIndex={50}
+      alignItems="center"
+      justifyContent="center"
+      elevationAndroid={1}
+    >
+      <Text color={isActive ? '$blue400' : '$white'}>{quantity}</Text>
+    </View>
+  )
+}
