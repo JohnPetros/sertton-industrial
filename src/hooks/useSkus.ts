af6fdae871d3fab74afb146ca955b9c071460e6f
João Pedro <joaopedro.nc@outlook.com>
2023-11-16 16:16:25 -0300

    ✨ feat (hooks): add multiple skus seleection logic to useSkus hook

--- src/hooks/useSkus.ts
+++ src/hooks/useSkus.ts
@@ -1,21 +1,16 @@
-import { useEffect, useState } from 'react'
+import { useRef, useState } from 'react'
 import { useQuery } from 'react-query'
 
 import type { Sku } from '@/@types/sku'
 import type { Variation } from '@/@types/variation'
 import { useApi } from '@/services/api'
-
-type VariationsByName = {
-  [name in string]: {
-    variations: Variation[]
-    selectedVariationValueId: number
-  }
-}
+import { removeObjectFromArray } from '@/utils/helpers/removeObjectFromArray'
 
 export function useSkus(productId: number) {
   const [selectedSku, setSelectedSku] = useState<Sku | null>(null)
-  const [variationsByName, setVariationsByName] =
-    useState<VariationsByName | null>(null)
+  const [variations, setVariations] = useState<Variation[]>([])
+  const variationNames = useRef<string[]>([])
+  const selectedVariationsValues = useRef<string[]>([])
   const api = useApi()
 
   const { data: skus } = useQuery(
@@ -26,13 +21,27 @@ export function useSkus(productId: number) {
     }
   )
 
-  function setVariations(skus: Sku[]) {
-    const variations: Variation[] = []
+  function getVariationValuesByVariationName(variationName: string) {
+    return (
+      variations
+        .filter((variation) => variation.name === variationName)
+        .map((variation) => variation.value) ?? []
+    )
+  }
+
+  function getAllVariations() {
+    if (!skus) return []
+
+    const allVariations: Variation[] = []
 
     for (const sku of skus) {
-      variations.push(...sku.variations)
+      allVariations.push(...sku.variations)
     }
 
+    return allVariations
+  }
+
+  function getVariationNames(variations: Variation[]) {
     const variationNames: string[] = []
 
     for (const variation of variations) {
@@ -41,92 +50,180 @@ export function useSkus(productId: number) {
       }
     }
 
-    let variationsByName = {}
-
-    for (const variantionName of variationNames) {
-      const selectedVariations: Variation[] = variations.filter(
-        (variantion) => (variantion.name = variantionName)
-      )
-
-      if (selectedVariations.length) {
-        variationsByName = {
-          ...variationsByName,
-          [variantionName]: {
-            variations: selectedVariations,
-            selectedVariationValueId: selectedVariations[0].value_id,
-          },
-        }
-      }
-    }
-
-    setVariationsByName(variationsByName)
+    return variationNames
   }
 
-  function getVariationByValue(value: string) {
-    if (variationsByName) {
-      const allVariations = []
+  function checkSkuMatch(
+    selectedVariationValue: string,
+    variationValue: string
+  ) {
+    if (!skus) return false
 
-      for (const { variations } of Object.values(variationsByName)) {
-        allVariations.push(...variations)
-      }
-
-      return allVariations.find((variation) => variation.value === value)
-    }
-  }
-
-  function handleSelectedVariationChange(variationValue: string) {
-    const selectedVariation = getVariationByValue(variationValue)
-
-    if (selectedVariation && variationsByName) {
-      const selectedVariationsByName = variationsByName[selectedVariation.name]
-
-      const updatedVariationsName = {
-        ...variationsByName,
-        [selectedVariation.name]: {
-          ...selectedVariationsByName,
-          selectedVariationValueId: selectedVariation.value_id,
-        },
-      }
-
-      setVariationsByName(updatedVariationsName)
-    }
-  }
-
-  function verifySkuValueIdMatch(sku: Sku) {
-    if (!variationsByName) return
-
-    for (const variationName of Object.keys(variationsByName)) {
+    return skus.some((sku) => {
       const selectedVariation = sku.variations.find(
-        (variation) => variation.name === variationName
+        (variation) => variation.value === selectedVariationValue
       )
 
-      const hasVariationValueMatch =
-        selectedVariation?.value_id ===
-        variationsByName[variationName].selectedVariationValueId
+      const currentVariation = sku.variations.find(
+        (variation) => variation.value === variationValue
+      )
 
-      if (!hasVariationValueMatch) return false
-    }
-
-    return true
+      if (selectedVariation && currentVariation) return true
+    })
   }
 
-  function selectSku() {
+  function removeRemainingVariations(firstVariationName: string) {
+    const allVariations = getAllVariations()
+
+    const nonRemainingvariations: Variation[] = []
+
+    for (const variation of allVariations) {
+      const isAlreadyIncluded = nonRemainingvariations.some(
+        (currentVariation) => currentVariation.value === variation.value
+      )
+      if (!isAlreadyIncluded && variation.name === firstVariationName) {
+        nonRemainingvariations.push(variation)
+      }
+    }
+
+    return nonRemainingvariations ? nonRemainingvariations : []
+  }
+
+  function insertRemainingVariations(
+    selectedVariationValue: string,
+    firstVariationName: string
+  ) {
+    const allVariations = getAllVariations()
+
+    const nonRemainingvariations = removeRemainingVariations(firstVariationName)
+
+    const remainingVariations = allVariations.filter(
+      (variation) => variation.name !== firstVariationName
+    )
+
+    const variations: Variation[] = []
+
+    for (const variation of remainingVariations) {
+      const isFirstVariationName = variation.name === firstVariationName
+      const shouldPush = !variations.some(
+        (currentCariation) => currentCariation.value === variation.value
+      )
+
+      const hasSkuMatch = checkSkuMatch(selectedVariationValue, variation.value)
+
+      if (!isFirstVariationName && shouldPush && hasSkuMatch) {
+        variations.push(variation)
+      }
+    }
+
+    setVariations(variations.concat(nonRemainingvariations))
+  }
+
+  function getFirstVariationName(
+    variationNames: string[],
+    allVariations: Variation[]
+  ) {
+    const variationNamesAmount = variationNames.length
+
+    for (const variation of allVariations) {
+      const variationsMatchesAmount = allVariations.filter(
+        (currentVariation) => currentVariation.value === variation.value
+      ).length
+
+      if (variationsMatchesAmount === variationNamesAmount) {
+        return variation.name
+      }
+    }
+
+    return variationNames[0]
+  }
+
+  function setSkusVariations(skus: Sku[]) {
+    const allVariations = getAllVariations()
+
+    if (!allVariations) return
+
+    variationNames.current = getVariationNames(allVariations)
+
+    const firstVariationName = getFirstVariationName(
+      variationNames.current,
+      allVariations
+    )
+
+    removeObjectFromArray<string>(variationNames.current, firstVariationName)
+
+    variationNames.current.unshift(firstVariationName)
+
+    const nonRemainingvariations = removeRemainingVariations(firstVariationName)
+    setVariations(nonRemainingvariations)
+
+    const selectedSku = skus[0]
+    setSelectedSku(selectedSku)
+  }
+
+  function handleSelectedVariationChange(
+    selectedVariationValue: string,
+    currentSelectedVariationValues: string[]
+  ) {
+    const selectedVariation = variations.find(
+      (variation) => variation.value === selectedVariationValue
+    )
+
+    if (selectedVariation) {
+      selectedVariationsValues.current = [
+        ...currentSelectedVariationValues,
+        selectedVariationValue,
+      ]
+      const selectedSku = getSelectedSku()
+
+      const allVariations = getAllVariations()
+
+      const firstVariationName = getFirstVariationName(
+        variationNames.current,
+        allVariations
+      )
+
+      const isFirstVariationName =
+        selectedVariation?.name === firstVariationName
+
+      if (isFirstVariationName) {
+        insertRemainingVariations(selectedVariationValue, firstVariationName)
+        return
+      }
+
+      console.log({ selectedSku })
+
+      if (selectedSku && skus)
+        setSelectedSku(selectedSku ? selectedSku : skus[0])
+    }
+  }
+
+  function verifyVariationValuesMatch(sku: Sku) {
+    const hasVariationValuesMatch = sku.variations.every((variation) => {
+      return selectedVariationsValues.current.includes(variation.value)
+    })
+
+    if (hasVariationValuesMatch) {
+      return true
+    }
+
+    return false
+  }
+
+  function getSelectedSku() {
     if (!skus) return
 
-    const selectedSku = skus.find(verifySkuValueIdMatch)
-
-    if (selectedSku) setSelectedSku(selectedSku)
+    return skus.find(verifyVariationValuesMatch) ?? skus[0]
   }
-
-  useEffect(() => {
-    if (variationsByName) selectSku()
-  }, [variationsByName])
 
   return {
     selectedSku,
     skus,
-    variationsByName,
-    setVariations,
+    variations,
+    variationNames: variationNames.current,
+    selectedVariationsValues: selectedVariationsValues.current,
+    getVariationValuesByVariationName,
+    setSkusVariations,
     handleSelectedVariationChange,
   }
 }
