a123df86a87513069d2691f4099881b206578c2f
João Pedro <joaopedro.nc@outlook.com>
2023-11-16 16:15:03 -0300

    ✨ feat (dx): add forwarded ref to Dialog component

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,8 +1,18 @@
-import { ReactNode } from 'react'
+import {
+  ForwardedRef,
+  forwardRef,
+  ReactNode,
+  useImperativeHandle,
+  useState,
+} from 'react'
 import { X } from 'phosphor-react-native'
 import { Dialog as D, XStack } from 'tamagui'
 
 import { Button } from '@/components/Button'
+
+export type DialogRef = {
+  close: () => void
+}
 
 interface DialogProps {
   title: string
@@ -11,14 +21,30 @@ interface DialogProps {
   onOpenChange?: (isOpen: boolean) => void
 }
 
-export function Dialog({
-  children,
-  content,
-  title,
-  onOpenChange,
-}: DialogProps) {
+export const DialogComponent = (
+  { children, content, title, onOpenChange }: DialogProps,
+  ref: ForwardedRef<DialogRef>
+) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  function handleOpenChange(isOpen: boolean) {
+    setIsOpen(isOpen)
+
+    if (onOpenChange) onOpenChange(isOpen)
+  }
+
+  function close() {
+    setIsOpen(false)
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      close,
+    }
+  })
+
   return (
-    <D onOpenChange={onOpenChange}>
+    <D open={isOpen} onOpenChange={handleOpenChange}>
       <D.Portal>
         <D.Overlay
           key="overlay"
@@ -27,9 +53,16 @@ export function Dialog({
           enterStyle={{ opacity: 0 }}
           exitStyle={{ opacity: 0 }}
         />
-        <D.Content py={16} px={24} bordered elevate key="content">
+        <D.Content
+          py={16}
+          px={24}
+          maxWidth="90%"
+          bordered
+          elevate
+          key="content"
+        >
           <XStack justifyContent="space-between">
-            <D.Title color="$gray900" fontWeight="600" fontSize={16}>
+            <D.Title fontSize={16} color="$gray900" fontWeight="600">
               {title}
             </D.Title>
             <D.Close asChild>
@@ -45,3 +78,5 @@ export function Dialog({
     </D>
   )
 }
+
+export const Dialog = forwardRef(DialogComponent)
