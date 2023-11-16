4a4500a33d23296e881538b3f474dbb0c6c74982
João Pedro <joaopedro.nc@outlook.com>
2023-11-16 16:20:24 -0300

    ✨ feat (ui): add lottie view to Loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react'
 import LottieView from 'lottie-react-native'
 import { Text, YStack } from 'tamagui'
 
@@ -5,21 +6,29 @@ import Truck from '@/assets/animations/truck.json'
 
 interface LoadingProps {
   message: string
+  size: number
 }
 
-export function Loading({ message }: LoadingProps) {
+export function Loading({ message, size }: LoadingProps) {
+  const animationRef = useRef<LottieView>(null)
+
+  useEffect(() => {
+    console.log('oi')
+    animationRef.current?.play()
+  }, [])
+
   return (
-    <YStack>
+    <YStack w="100%" alignItems="center" justifyContent="center">
       <LottieView
         style={{
-          width: 50,
-          height: 50,
-          justifyContent: 'center',
-          alignItems: 'center',
+          width: size,
+          height: size,
+          zIndex: 50,
         }}
-        source={require('@/assets/animations/truck.json')}
+        loop
+        source={require('../assets/animations/truck.json')}
       />
-      <Text color="$blue200" fontSize={12}>
+      <Text color="$blue200" fontSize={16} fontWeight="600">
         {message}
       </Text>
     </YStack>
