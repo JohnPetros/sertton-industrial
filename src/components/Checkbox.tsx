47838b6522c85673a81748e6cc3e0f350841bf53
João Pedro <joaopedro.nc@outlook.com>
2023-11-18 13:45:51 -0300

    🏷️ type: add defaultChecked prop to Checkbox component

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -5,19 +5,25 @@ import { Checkbox as Box, getTokens, Label, XStack } from 'tamagui'
 interface CheckboxProps {
   label: string
   value: string
-  onChange: (value: string) => void
+  defaultChecked: boolean
+  onChange: (value: string, isChecked: boolean) => void
 }
 
-export function Checkbox({ label, value, onChange }: CheckboxProps) {
-  const [isChecked, setIsChecked] = useState(false)
+export function Checkbox({
+  label,
+  value,
+  defaultChecked,
+  onChange,
+}: CheckboxProps) {
+  const [isChecked, setIsChecked] = useState(defaultChecked)
   const id = useId()
 
   function handleCheckedChange(isChecked: boolean) {
     setIsChecked(isChecked)
-    onChange(value)
+    onChange(value, isChecked)
   }
   return (
-    <XStack gap={12}>
+    <XStack alignItems="center">
       <Box
         id={id}
         borderRadius={2}
@@ -25,12 +31,15 @@ export function Checkbox({ label, value, onChange }: CheckboxProps) {
         onCheckedChange={handleCheckedChange}
         bg={isChecked ? '$blue400' : '$colorTransparent'}
         size="$5"
+        checked={isChecked}
       >
         <Box.Indicator>
           <Check color={getTokens().color.white.val} />
         </Box.Indicator>
       </Box>
-      <Label htmlFor={id}>{label}</Label>
+      <Label htmlFor={id} p={8}>
+        {label}
+      </Label>
     </XStack>
   )
 }
