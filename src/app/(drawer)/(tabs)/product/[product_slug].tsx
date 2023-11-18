e7fe3b229ba323bd255af3bcc4c8f298fd2f6e74
João Pedro <joaopedro.nc@outlook.com>
2023-11-18 13:45:03 -0300

    ✨ feat: show product description in product screen

--- src/app/(drawer)/(tabs)/product/[product_slug].tsx
+++ src/app/(drawer)/(tabs)/product/[product_slug].tsx
@@ -2,7 +2,15 @@ import { useCallback, useEffect, useRef, useState } from 'react'
 import { useFocusEffect, useNavigation, useRouter } from 'expo-router'
 import { useGlobalSearchParams } from 'expo-router/src/hooks'
 import { ArrowsOut } from 'phosphor-react-native'
-import { getTokens, H2, ScrollView, View, XStack, YStack } from 'tamagui'
+import {
+  getTokens,
+  H2,
+  Paragraph,
+  ScrollView,
+  View,
+  XStack,
+  YStack,
+} from 'tamagui'
 
 import type { Sku } from '@/@types/sku'
 import { Button } from '@/components/Button'
@@ -26,6 +34,7 @@ import { useCartStore } from '@/stores/cartStore'
 import { ROUTES } from '@/utils/constants/routes'
 import { SCREEN } from '@/utils/constants/screen'
 import { TAB_BAR_HEIGHT } from '@/utils/constants/tabBarHeight'
+import { removeHTMLTags } from '@/utils/helpers/removeHTMLTags'
 
 export default function Product() {
   const { product_slug } = useGlobalSearchParams()
@@ -33,8 +42,8 @@ export default function Product() {
   const [isLoading, setIsLoading] = useState(true)
   const [quantity, setQuantity] = useState(1)
   const [selectedSku, setSelectedSku] = useState<Sku | null>(null)
+  const [isFullImageVisible, setIsFullImageVisible] = useState(false)
   const skuSelectsRef = useRef<SkuSelectsRef | null>(null)
-  console.log(product?.description)
 
   const {
     state: { items },
@@ -42,8 +51,9 @@ export default function Product() {
   } = useCartStore()
   const router = useRouter()
   const navigation = useNavigation()
-
-  const [isFullImageVisible, setIsFullImageVisible] = useState(false)
+  const hasVariations = Boolean(
+    skuSelectsRef.current?.selectedSku?.variations.length
+  )
 
   const item = items.find((item) => item.slug === product?.slug)
   const isInCart = !!item
@@ -67,7 +77,7 @@ export default function Product() {
   function handleAddToCart() {
     const shouldAddTocart = skuSelectsRef.current?.onAddSkuToCart()
 
-    if (!shouldAddTocart) return
+    if (hasVariations && !shouldAddTocart) return
 
     if (!isInCart && product && selectedSku) {
       addItem({ slug: product.slug, skuId: selectedSku.id, quantity })
@@ -103,7 +113,7 @@ export default function Product() {
 
     const timer = setTimeout(() => {
       setIsLoading(false)
-    }, 500)
+    }, 100)
 
     return () => clearTimeout(timer)
   }, [selectedSku])
@@ -161,42 +171,37 @@ export default function Product() {
           </View>
         </Skeleton>
         <YStack px={SCREEN.paddingX} mt={12} gap={8}>
-          {selectedSku && (
-            <>
-              <Skeleton isVisible={isSkeletonVisible} width={120} height={24}>
-                {selectedSku && (
-                  <SkuCode fontSize={14}>{selectedSku?.sku}</SkuCode>
-                )}
-              </Skeleton>
-              <Skeleton isVisible={isSkeletonVisible} width={300} height={48}>
-                {selectedSku && (
-                  <Name fontSize={24}>{String(product?.name)}</Name>
-                )}
-              </Skeleton>
+          <>
+            <Skeleton isVisible={isSkeletonVisible} width={120} height={24}>
+              {selectedSku && (
+                <SkuCode fontSize={14}>{selectedSku?.sku}</SkuCode>
+              )}
+            </Skeleton>
+            <Skeleton isVisible={isSkeletonVisible} width={300} height={48}>
+              {selectedSku && (
+                <Name fontSize={24}>{String(product?.name)}</Name>
+              )}
+            </Skeleton>
 
-              <Skeleton isVisible={isSkeletonVisible} width={150} height={48}>
-                {selectedSku && (
-                  <XStack alignItems="flex-start" gap={12}>
-                    <YStack>
-                      <SalePrice
-                        fontSize={24}
-                        price={selectedSku?.price_sale}
-                      />
-                      <DiscountPrice
-                        fontSize={14}
-                        price={selectedSku?.price_discount}
-                      />
-                    </YStack>
-                    <Discount
-                      salesPrice={selectedSku?.price_sale}
-                      discountPrice={selectedSku?.price_discount}
+            <Skeleton isVisible={isSkeletonVisible} width={150} height={48}>
+              {selectedSku && (
+                <XStack alignItems="flex-start" gap={12}>
+                  <YStack>
+                    <SalePrice fontSize={24} price={selectedSku?.price_sale} />
+                    <DiscountPrice
+                      fontSize={14}
+                      price={selectedSku?.price_discount}
                     />
-                  </XStack>
-                )}
-              </Skeleton>
-            </>
-          )}
-          <YStack mt={12} gap={32} alignItems="flex-start">
+                  </YStack>
+                  <Discount
+                    salesPrice={selectedSku?.price_sale}
+                    discountPrice={selectedSku?.price_discount}
+                  />
+                </XStack>
+              )}
+            </Skeleton>
+          </>
+          <YStack mt={hasVariations ? 12 : 0} gap={32} alignItems="flex-start">
             {!product ? (
               <Skeleton isVisible={!product} width={SCREEN.width} height={40}>
                 <></>
@@ -220,19 +225,15 @@ export default function Product() {
                 onChangeNumber={handleQuantityChange}
               />
             </Skeleton>
-            {isSkeletonVisible ? (
-              <Skeleton
-                isVisible={isSkeletonVisible}
-                width={SCREEN.width}
-                height={40}
-              >
-                <></>
-              </Skeleton>
-            ) : (
+            <Skeleton
+              isVisible={isSkeletonVisible}
+              width={SCREEN.width}
+              height={40}
+            >
               <Button w="100%" onPress={handleAddToCart}>
                 Adicionar ao carinho
               </Button>
-            )}
+            </Skeleton>
           </YStack>
 
           {selectedSku && (
@@ -251,9 +252,12 @@ export default function Product() {
             </View>
           )}
 
-          {selectedSku && !isLoading && (
+          {product && !isLoading && (
             <YStack mt={24}>
-              <H2>Descrição do produto</H2>
+              <H2 fontSize={24}>Descrição do produto</H2>
+              <Paragraph lineHeight={28}>
+                {removeHTMLTags(product.texts.data.description)}
+              </Paragraph>
             </YStack>
           )}
         </YStack>
