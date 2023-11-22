62116c9ca93791703e0e8568efa5c4349d84169a
João Pedro <joaopedro.nc@outlook.com>
2023-11-22 20:56:49 -0300

    🏷️ type: add Meta type

--- src/@types/api.ts
+++ src/@types/api.ts
@@ -1,6 +1,16 @@
 import { AxiosResponse } from 'axios'
 
-type Response<T> = Promise<AxiosResponse<{ data: T }>>
+type Meta = {
+  pagination: {
+    total: number
+    count: number
+    per_page: number
+    current_page: number
+    total_pages: number
+  }
+}
+
+type Response<T> = Promise<AxiosResponse<{ data: T; meta: Meta }>>
 
 export type Api = {
   get: <T>(url: string) => Response<T>
