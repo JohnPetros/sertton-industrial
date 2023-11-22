d8a364aebc12d3bc423880f3cd0c68dd7653e76e
João Pedro <joaopedro.nc@outlook.com>
2023-11-22 20:59:09 -0300

    ✨ feat (storage): add cartStorage

---
+++ src/services/storage/cartStorage.ts
@@ -0,0 +1,19 @@
+import type { Storage } from '@/@types/storage'
+import { ICartStorage } from '@/services/storage/interfaces/ICartStorage'
+
+export function cartStorage(storage: Storage): ICartStorage {
+  return {
+    async getItem(name) {
+      const item = await storage.get(name)
+      return item ? item : null
+    },
+
+    async removeItem(name) {
+      storage.delete(name)
+    },
+
+    async setItem(name, value) {
+      storage.set(name, value)
+    },
+  }
+}
