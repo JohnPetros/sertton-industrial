28073af056c3b94cc98c7611fe0aea4d2745c289
João Pedro <joaopedro.nc@outlook.com>
2023-11-22 20:59:57 -0300

    ✨ feat (storage): add mmkv storage

---
+++ src/services/storage/mmkv.ts
@@ -0,0 +1,22 @@
+import { MMKV } from 'react-native-mmkv'
+
+import type { Storage } from '@/@types/storage'
+import { STORAGE_ID } from '@/services/storage/keys'
+
+const storage = new MMKV({ id: STORAGE_ID })
+
+export const mmkv: Storage = {
+  async get(key: string): Promise<string | null> {
+    const item = storage.getString(key)
+    if (item) item
+    return null
+  },
+
+  async set(key: string, value: string) {
+    storage.set(key, value)
+  },
+
+  async delete(key: string) {
+    storage.delete(key)
+  },
+}
