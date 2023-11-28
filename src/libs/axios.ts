d930908f9a25fe72063daca70ff5e1db2799e49b
João Pedro <joaopedro.nc@outlook.com>
2023-11-28 10:45:50 -0300

    ✨ feat (libs): add axios lib

---
+++ src/libs/axios.ts
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+import type { Api } from '@/@types/api'
+
+const BASE_URL = process.env.YAMPI_BASE_URL
+const ALIAS = process.env.ALIAS
+
+const axiosClient = axios.create({
+  baseURL: `${BASE_URL}/${ALIAS}`,
+  headers: {
+    'User-Token': process.env.YAMPI_TOKEN,
+    'User-Secret-Key': process.env.YAMPI_SECRET_KEY,
+  },
+})
+
+export const axiosApi: Api = {
+  async get(url) {
+    const { data } = await axiosClient.get(url)
+    return data
+  },
+  async post<Request, Response>(url: string, request: Request) {
+    return (await axiosClient.post(url, request)) as Response
+  },
+  async put<Request, Response>(url: string, request: Request) {
+    return (await axiosClient.put(url, request)) as Response
+  },
+  async delete<Response>(url: string) {
+    return (await axiosClient.delete(url)) as Response
+  },
+}
