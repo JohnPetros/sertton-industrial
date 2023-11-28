2c5cd642f2ba2a7db8a480c1726eab972fdf508b
João Pedro <joaopedro.nc@outlook.com>
2023-11-28 16:39:12 -0300

    ✨ feat (ui): add Heading component

---
+++ src/components/CheckoutForm/Heading.tsx
@@ -0,0 +1,32 @@
+import { Text, View, XStack, YStack } from 'tamagui'
+
+interface TitleProps {
+  step: number
+  title: string
+  subtitle: string
+}
+
+export function Heading({ step, title, subtitle }: TitleProps) {
+  return (
+    <YStack gap={8}>
+      <XStack gap={8} alignItems="center">
+        <View
+          w={32}
+          h={32}
+          alignItems="center"
+          justifyContent="center"
+          borderRadius={16}
+          bg="$blue600"
+        >
+          <Text color="$white" fontWeight="600">
+            {step}
+          </Text>
+        </View>
+        <Text color="$blue500" fontWeight="600">
+          {title}
+        </Text>
+      </XStack>
+      <Text>{subtitle}</Text>
+    </YStack>
+  )
+}
