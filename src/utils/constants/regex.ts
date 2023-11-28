595b6d658907abcc2a2093ca90c8f929d7835aa6
João Pedro <joaopedro.nc@outlook.com>
2023-11-28 16:37:15 -0300

    ✨ feat (utils): add cpf and phone regex

--- src/utils/constants/regex.ts
+++ src/utils/constants/regex.ts
@@ -2,4 +2,6 @@ export const REGEX = {
   htmlTags: /<\/?[^>]+(>|$)/g,
   htmlList: /<li>(.*?)<\/li>/g,
   htmlLiTag: /<\/?li>/g,
+  cpf: /^\d{3}\.\d{3}\.\d{3}-\d{2}$/,
+  phone: /^\d{3}\.\d{3}\.\d{3}-\d{2}$/,
 }
