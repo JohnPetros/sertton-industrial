33bbe8a34a14469677a02d6d8a597f5d3608675c
João Pedro <joaopedro.nc@outlook.com>
2023-12-04 18:26:36 -0300

    ✨ feat (utils): add email and password regex

--- src/utils/constants/regex.ts
+++ src/utils/constants/regex.ts
@@ -4,4 +4,6 @@ export const REGEX = {
   htmlLiTag: /<\/?li>/g,
   cpf: /^\d{3}\.\d{3}\.\d{3}-\d{2}$/,
   phone: /^\d{3}\.\d{3}\.\d{3}-\d{2}$/,
+  email: /^\S+@\S+\.\S+$/,
+  password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s])[A-Za-z\d\W\S]{6,}$/g,
 }
