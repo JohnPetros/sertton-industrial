2e65b82322fca5de398b60619000781d0d4ee0c8
João Pedro <joaopedro.nc@outlook.com>
2023-12-05 09:19:56 -0300

    🏷️ type: add Customer type

---
+++ src/@types/customer.ts
@@ -0,0 +1,10 @@
+export type Customer = {
+  id: number
+  name: string
+  email: string
+  razao_social?: string
+  active: boolean
+  password: string
+  password_confirmation: string
+  homephone: string
+}
