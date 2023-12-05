9d5b1e5938833f4081b1ccff0b88abf63a60f520
João Pedro <joaopedro.nc@outlook.com>
2023-12-05 12:49:46 -0300

    ✨ feat (libs): add fullname regex to zod name schema

--- src/libs/zod.ts
+++ src/libs/zod.ts
@@ -2,20 +2,26 @@ import { z } from 'zod'
 
 import { REGEX } from '@/utils/constants/regex'
 
-const nameSchema = z.string({
-  required_error: 'Campo obrigatório',
-})
+const nameSchema = z
+  .string({
+    required_error: 'Campo obrigatório',
+  })
+  .regex(REGEX.fullname, 'Digite seu nome completo')
+
 const emailSchema = z
   .string({
     required_error: 'Campo obrigatório',
   })
   .regex(REGEX.email, 'E-mail inválido')
+
 const cpfSchema = z.string({
   required_error: 'Campo obrigatório',
 })
+
 const cnpjSchema = z.string({
   required_error: 'Campo obrigatório',
 })
+
 const phoneSchema = z.string({
   required_error: 'Campo obrigatório',
 })
@@ -33,12 +39,16 @@ const passwordSchema = z
     'Senha deve conter pelo menos uma letra minúscula, uma maiúscula, um dígito e um caractere especial.'
   )
 
+const passwordConfirmationSchema = z.string({
+  required_error: 'Campo obrigatório',
+})
+
 const naturalPersonFormSchema = z
   .object({
     name: nameSchema,
     email: emailSchema,
     password: passwordSchema,
-    passwordConfirmation: z.string(),
+    passwordConfirmation: passwordConfirmationSchema,
     cpf: cpfSchema,
     phone: phoneSchema,
   })
@@ -52,7 +62,7 @@ const legalPersonFormSchema = z
     razaoSocial: razaoSocialSchema,
     email: emailSchema,
     password: passwordSchema,
-    passwordConfirmation: z.string(),
+    passwordConfirmation: passwordConfirmationSchema,
     cnpj: cnpjSchema,
     phone: phoneSchema,
   })
