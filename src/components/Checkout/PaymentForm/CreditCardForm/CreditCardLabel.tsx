3585bda21e535ab6815f861c9973facc69edd5bf
João Pedro <joaopedro.nc@outlook.com>
2023-12-11 12:34:42 -0300

    💄 style: add icon to Credit Card Label

--- src/components/Checkout/PaymentForm/CreditCardForm/CreditCardLabel.tsx
+++ src/components/Checkout/PaymentForm/CreditCardForm/CreditCardLabel.tsx
@@ -1,11 +1,15 @@
-import { Image, Text, XStack, YStack } from 'tamagui'
+import { CreditCard } from 'phosphor-react-native'
+import { getTokens, Image, Text, XStack, YStack } from 'tamagui'
 
 export function CreditCardLabel() {
   return (
     <YStack gap={12}>
-      <Text fontWeight="600" color="$gray900">
-        Cartão de Crédito
-      </Text>
+      <XStack gap={8}>
+        <CreditCard color={getTokens().color.gray800.val} />
+        <Text fontWeight="600" color="$gray900">
+          Cartão de Crédito
+        </Text>
+      </XStack>
       {/* <XStack gap={8}>
         <Image source={require('@/assets/icons/visa.svg')} />
         <Image source={require('@/assets/icons/visa.svg')} />
