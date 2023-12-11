a16b52498a305d1602b13621544e4ba206fea15b
João Pedro <joaopedro.nc@outlook.com>
2023-12-11 17:06:51 -0300

    ✨ feat (hooks): add useShipmentServiceForm hook

---
+++ src/components/CheckoutForm/ShipmentServiceForm/useShipmentServiceForm.ts
@@ -0,0 +1,62 @@
+import { useQuery } from 'react-query'
+
+import { useCart } from '@/hooks/useCart'
+import { useApi } from '@/services/api'
+import { useStorage } from '@/services/storage'
+import { useCheckoutStore } from '@/stores/checkoutStore'
+
+export function useShipmentServiceForm() {
+  const storage = useStorage()
+
+  const api = useApi()
+
+  const { products: cartProducts, getSelectedSkus } = useCart()
+
+  const setShipmentService = useCheckoutStore(
+    (store) => store.actions.setShipmentService
+  )
+  const storedShipmentService = useCheckoutStore(
+    (store) => store.state.shipmentService
+  )
+
+  async function getShipmentServices() {
+    const selectedAddressZipcode =
+      await storage.getCustomerSelectedAddressZipcode()
+
+    if (!selectedAddressZipcode || !cartProducts) return
+
+    const selectedSkus = getSelectedSkus()
+
+    if (selectedSkus) {
+      try {
+        return await api.getShipmentServices(
+          selectedAddressZipcode,
+          selectedSkus
+        )
+      } catch (error) {
+        api.handleError(error)
+      }
+    }
+  }
+
+  const { data: shipmentServices } = useQuery(
+    ['shipment-services'],
+    getShipmentServices
+  )
+
+  function handleShipmentServiceChange(shipmentServiceName: string) {
+    if (!shipmentServices) return
+
+    const selectedShipmentService = shipmentServices.find(
+      (shipmentService) => shipmentService.name === shipmentServiceName
+    )
+
+    setShipmentService(selectedShipmentService)
+  }
+
+  return {
+    shipmentServices,
+    selectedShipmentService: storedShipmentService,
+    handleShipmentServiceChange,
+  }
+}
