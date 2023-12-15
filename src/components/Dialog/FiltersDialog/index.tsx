7423e7bbcba2c1e1fadc69387201889d67015bce
João Pedro <joaopedro.nc@outlook.com>
2023-12-14 22:14:24 -0300

    ✨ feat (hooks): add useFiltersDialog hook

---
+++ src/components/Dialog/FiltersDialog/index.tsx
@@ -0,0 +1,59 @@
+import { ReactNode } from 'react'
+import { Text, YGroup, YStack } from 'tamagui'
+
+import type { Brand } from '@/@types/brand'
+import { Button } from '@/components/Button'
+import { Checkbox } from '@/components/Checkbox'
+import { Dialog } from '@/components/Dialog'
+import { useFiltersDialog } from '@/components/Dialog/FiltersDialog/useFiltersDialog'
+
+interface FiltersDialogProps {
+  brands: Brand[]
+  children: ReactNode
+}
+
+export function FiltersDialog({ children, brands }: FiltersDialogProps) {
+  const {
+    checkedBrandsIds,
+    dialogRef,
+    handleBrandCheckbox,
+    handleDialogOpenChange,
+    handleFilterButton,
+  } = useFiltersDialog(brands)
+
+  return (
+    <Dialog
+      ref={dialogRef}
+      onOpenChange={handleDialogOpenChange}
+      title="Filtrar por"
+      width={320}
+      content={
+        <YStack mt={12} pb={12}>
+          <YGroup>
+            <YGroup.Item>
+              <Text fontSize={16} fontWeight="600" color="$gray900" mb={4}>
+                Marca
+              </Text>
+            </YGroup.Item>
+            {brands &&
+              brands.map((brand) => (
+                <YGroup.Item key={brand.id}>
+                  <Checkbox
+                    value={String(brand.id)}
+                    label={brand.name}
+                    onChange={handleBrandCheckbox}
+                    defaultChecked={checkedBrandsIds.includes(brand.id)}
+                  />
+                </YGroup.Item>
+              ))}
+          </YGroup>
+          <Button onPress={handleFilterButton} mt={24}>
+            Filtrar produtos
+          </Button>
+        </YStack>
+      }
+    >
+      {children}
+    </Dialog>
+  )
+}
---
+++ src/components/Dialog/FiltersDialog/useFiltersDialog.ts
@@ -0,0 +1,60 @@
+import { useRef } from 'react'
+
+import { Brand } from '@/@types/brand'
+import { DialogRef } from '@/components/Dialog'
+import { useProductsFilterStore } from '@/stores/productsFilterStore'
+
+export function useFiltersDialog(brands: Brand[]) {
+  const checkedBrandsIds = useProductsFilterStore(
+    (store) => store.state.brandsIds
+  )
+  const setProductBrandsIds = useProductsFilterStore(
+    (store) => store.actions.setBrandsIds
+  )
+  const brandsIds = useRef<number[]>(checkedBrandsIds)
+  const dialogRef = useRef<DialogRef | null>(null)
+
+  function addBrandId(id: number) {
+    brandsIds.current = [...brandsIds.current, id]
+  }
+
+  function removeBrandId(id: number) {
+    brandsIds.current = brandsIds.current.filter((brandId) => brandId !== id)
+  }
+
+  function handleBrandCheckbox(brandId: string, isChecked: boolean) {
+    const id = Number(brandId)
+
+    if (isChecked) {
+      addBrandId(id)
+    } else if (!isChecked) {
+      removeBrandId(id)
+    }
+  }
+
+  function handleDialogOpenChange() {
+    brandsIds.current = checkedBrandsIds
+  }
+
+  async function handleFilterButton() {
+    console.log({ checkedBrandsIds })
+
+    dialogRef.current?.close()
+
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        setProductBrandsIds(brandsIds.current)
+        resolve(true)
+      }, 500)
+    })
+  }
+
+  return {
+    brands,
+    checkedBrandsIds,
+    dialogRef,
+    handleBrandCheckbox,
+    handleDialogOpenChange,
+    handleFilterButton,
+  }
+}
