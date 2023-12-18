fa0b3056fdc58f2dc7d5e050c366f295a6765bc4
João Pedro <joaopedro.nc@outlook.com>
2023-12-18 14:42:00 -0300

    🏷️ type: add PaymentConfig type

--- src/@types/paymentMethod.ts
+++ src/@types/paymentMethod.ts
@@ -1,1 +1,16 @@
 export type PaymentMethod = 'credit-card' | 'ticket' | 'pix'
+
+export type PaymentConfig = {
+  id: number
+  name: string
+  alias: string
+  has_config: boolean
+  active_config: boolean
+  is_credit_card: boolean
+  is_deposit: boolean
+  is_billet: boolean
+  is_pix: boolean
+  is_pix_in_installments: boolean
+  is_wallet: boolean
+  icon_url: string
+}
