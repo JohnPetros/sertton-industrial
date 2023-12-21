eeaa14ce8f8cc0cc90851ebc88be409e62f008da
João Pedro <joaopedro.nc@outlook.com>
2023-12-20 22:16:55 -0300

    ✨ feat (api): getOrdersByCustomerDocument

--- src/services/api/interfaces/IOrdersController.ts
+++ src/services/api/interfaces/IOrdersController.ts
@@ -1,4 +1,6 @@
-import { Order } from '@/@types/order'
+import type { ComputedOrder } from '@/@types/computedOrder'
+import type { Order } from '@/@types/order'
 export interface IOrdersController {
   saveOrder(order: Order): Promise<void>
+  getOrdersByCustomerDocument(document: string): Promise<ComputedOrder[]>
 }
--- src/services/api/ordersController.ts
+++ src/services/api/ordersController.ts
@@ -1,4 +1,5 @@
 import type { Api } from '@/@types/api'
+import { ComputedOrder } from '@/@types/computedOrder'
 import type { Order } from '@/@types/order'
 import { IOrdersController } from '@/services/api/interfaces/IOrdersController'
 import { Resources } from '@/services/api/resources'
@@ -7,6 +8,13 @@ export function ordersController(api: Api): IOrdersController {
   return {
     async saveOrder(order: Order) {
       await api.post(`/${Resources.ORDERS}`, order)
+    },
+
+    async getOrdersByCustomerDocument(document: string) {
+      const response = await api.get<{ data: ComputedOrder[] }>(
+        `/${Resources.ORDERS}?q=${document}`
+      )
+      return response.data
     },
   }
 }
