60217cbff8ba4787a597f13eb13ae18d72c04239
João Pedro <joaopedro.nc@outlook.com>
2023-12-20 22:13:52 -0300

    ✨ feat (hooks): add useOrderItem hook

---
+++ src/components/OrdersList/OrderItem/useOrderItem.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+
+import type { ComputedOrder } from '@/@types/computedOrder'
+
+export function useOrderItem(order: ComputedOrder) {
+  const [totalDiscount, setTotalDiscount] = useState(0)
+  const [subtotal, setSubtotal] = useState(0)
+  const [skusAmount, setSkusAmount] = useState(0)
+
+  function calculateTotalDiscount() {
+    const totalDiscount = order.items.data.reduce((total, item) => {
+      return (
+        total +
+        (item.sku.data.price_sale - item.sku.data.price_discount) *
+          item.quantity
+      )
+    }, 0)
+
+    const subtotal = order.items.data.reduce((total, item) => {
+      return total + item.sku.data.price_sale * item.quantity
+    }, 0)
+
+    const skusAmount = order.items.data.reduce((total, item) => {
+      return total + item.quantity
+    }, 0)
+
+    setTotalDiscount(totalDiscount)
+    setSubtotal(subtotal)
+    setSkusAmount(skusAmount)
+  }
+
+  useEffect(() => {
+    calculateTotalDiscount()
+  }, [])
+
+  return {
+    subtotal,
+    totalDiscount,
+    skusAmount,
+  }
+}
