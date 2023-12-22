d25ce407b3e1a83e674c8a024d5ead365cee67a4
João Pedro <joaopedro.nc@outlook.com>
2023-12-22 16:05:42 -0300

    🧪 test: add ordersMock

---
+++ src/@types/mock.ts
@@ -0,0 +1,415 @@
+// {
+//   "marketplace_id": null,
+//   "metadata": {
+//     "data": [
+//       {
+//         "value": "store_v2",
+//         "key": "source_platform"
+//       }
+//     ]
+//   },
+//   "marketplace_sale_number": null,
+//   "marketplace_partner_id": null,
+//   "utm_medium": null,
+//   "delivered": false,
+//   "content_statement_url": "https://api.dooki.com.br/html/orders/content-statement/60c61140-9dc7-11ee-9a01-022cbdca293e-65807b66422e2?id=def50200e905b045da248f25474a7be502090f5b29aba43ed76429fce353ca79c32c6700306e02d85ebedb2283edc4f5177282c9e62dbb2218d75cd058ae119494d3f4f9f9d47c2656cc948b7da84ae0d2b627c0d10710c64ccf5858fb",
+//   "buyer_value_tax": 0,
+//   "marketplace_account": {
+//     "data": []
+//   },
+//   "is_upsell": false,
+//   "desire_status_id": [
+//     8,
+//     4,
+//     9
+//   ],
+//   "value_products": 124.52,
+//   "public_url": "https://api.dooki.com.br/v2/public/orders/60c61140-9dc7-11ee-9a01-022cbdca293e-65807b66422e2",
+//   "number": 739383970908649,
+//   "status_id": 3,
+//   "authorized": false,
+//   "id": 102855369,
+//   "billet_whatsapp_app_link": "whatsapp://send?phone=5512988815499&text=Aqui está o boleto do produto *Dobradiça com Mancal e pino - Pequena - 3 furos Inox*, no valor de R$ 136,29%0a%0aVencimento: *20/12/2023*%0a%0aCódigo de barras: *23791229286001337720991000046903295700000013629*%0a%0aLink: https%3A%2F%2Fapi.pagar.me%2F1%2Fboletos%2Flive_clqcf4ctg5atb01m57ejewat6%3Fformat%3Dpdf",
+//   "pix": {
+//     "data": []
+//   },
+//   "marketplace": {
+//     "data": []
+//   },
+//   "has_recomm": false,
+//   "utm_campaign": null,
+//   "shipment_service": "ME_SEDEX_CENTRALIZADO_29",
+//   "ip": "187.95.31.48",
+//   "promocode": {
+//     "data": []
+//   },
+//   "transactions": {
+//     "data": [
+//       {
+//         "marketplace_id": null,
+//         "cancelled_at": null,
+//         "metadata": {
+//           "data": []
+//         },
+//         "captured_at": null,
+//         "billet_barcode": "23791.22928 60013.377209 91000.046903 2 95700000013629",
+//         "billet_url": "https://api.pagar.me/1/boletos/live_clqcf4ctg5atb01m57ejewat6?format=pdf",
+//         "gateway_billet_id": null,
+//         "installments": 1,
+//         "authorized": true,
+//         "payment": {
+//           "data": {
+//             "icon_url": "https://icons.yampi.me/svg/card-billet.svg",
+//             "active_config": false,
+//             "is_pix": false,
+//             "is_credit_card": false,
+//             "is_pix_in_installments": false,
+//             "is_billet": true,
+//             "name": "Boleto Bancário",
+//             "alias": "billet",
+//             "id": 9,
+//             "has_config": false,
+//             "is_deposit": false,
+//             "is_wallet": false
+//           }
+//         },
+//         "id": 121593397,
+//         "buyer_installment_formated": "1x de R$ 0,00",
+//         "bank_alias": null,
+//         "error_message": null,
+//         "billet_document_number": null,
+//         "gateway_order_id": "or_jXA5qJLsBs3YazZ2",
+//         "antifraud_score": null,
+//         "installment_value": 136.29,
+//         "capture_date": null,
+//         "antifraud_status": null,
+//         "cancelled": false,
+//         "can_be_cancelled": true,
+//         "error_code": null,
+//         "total_logs": 0,
+//         "billet_our_number": null,
+//         "marketplace_account_id": null,
+//         "status": "waiting_payment",
+//         "antifraud_sale_id": null,
+//         "buyer_amount": 0,
+//         "created_at": {
+//           "date": "2023-12-19 11:06:19.000000",
+//           "timezone": "America/Sao_Paulo",
+//           "timezone_type": 3
+//         },
+//         "affiliation_id": 539540,
+//         "authorized_at": {
+//           "date": "2023-12-19 11:06:19.000000",
+//           "timezone": "America/Sao_Paulo",
+//           "timezone_type": 3
+//         },
+//         "installment_formated": "1x de R$ 136,29",
+//         "gateway_authorization_code": null,
+//         "updated_at": {
+//           "date": "2023-12-19 11:06:19.000000",
+//           "timezone": "America/Sao_Paulo",
+//           "timezone_type": 3
+//         },
+//         "payment_id": 9,
+//         "captured": false,
+//         "billet_whatsapp_link": "https://api.whatsapp.com/send?phone=5512988815499&text=Aqui está o boleto do produto *Dobradiça com Mancal e pino - Pequena - 3 furos Inox*, no valor de R$ 136,29%0a%0aVencimento: *20/12/2023*%0a%0aCódigo de barras: *23791229286001337720991000046903295700000013629*%0a%0aLink: https%3A%2F%2Fapi.pagar.me%2F1%2Fboletos%2Flive_clqcf4ctg5atb01m57ejewat6%3Fformat%3Dpdf",
+//         "bank_name": null,
+//         "holder_name": null,
+//         "amount": 136.29,
+//         "holder_document": null,
+//         "sent_to_antifraud": false,
+//         "billet_date": {
+//           "date": "2023-12-20 00:00:00.000000",
+//           "timezone": "America/Sao_Paulo",
+//           "timezone_type": 3
+//         },
+//         "gateway_transaction_id": "ch_NKP1bVafGf2ZG6Qy",
+//         "can_be_captured": true,
+//         "customer_id": 179128559,
+//         "truncated_card": null,
+//         "buyer_installment_value": 0
+//       }
+//     ]
+//   },
+//   "value_discount": 0,
+//   "shipment_service_id": "29",
+//   "statuses": {
+//     "data": [
+//       {
+//         "updated_at": {
+//           "date": "2023-12-19 11:06:19.000000",
+//           "timezone": "America/Sao_Paulo",
+//           "timezone_type": 3
+//         },
+//         "name": "Aguardando pagamento",
+//         "alias": "waiting_payment",
+//         "description": "Aguardando confirmação de pagamento",
+//         "created_at": {
+//           "date": "2023-12-19 11:06:19.000000",
+//           "timezone": "America/Sao_Paulo",
+//           "timezone_type": 3
+//         },
+//         "details": null,
+//         "id": 3
+//       }
+//     ]
+//   },
+//   "cart_token": "60c61140-9dc7-11ee-9a01-022cbdca293e-65807b66422e2",
+//   "value_tax": 0,
+//   "device": "desktop",
+//   "items": {
+//     "data": [
+//       {
+//         "gift": false,
+//         "price_cost": 62.26,
+//         "quantity": 1,
+//         "item_sku": "116000003P",
+//         "has_recomm": 0,
+//         "is_digital": false,
+//         "sku_id": 151463680,
+//         "shipment_cost": 11.77,
+//         "bundle_name": null,
+//         "gift_value": 0,
+//         "price": 124.52,
+//         "product_id": 22194035,
+//         "bundle_id": null,
+//         "id": 124473304,
+//         "sku": {
+//           "data": {
+//             "price_cost": 62.26,
+//             "days_availability_formated": "Imediata",
+//             "created_at": {
+//               "date": "2023-09-05 11:27:22.000000",
+//               "timezone": "America/Sao_Paulo",
+//               "timezone_type": 3
+//             },
+//             "availability": 0,
+//             "title": "Dobradiça com Mancal e pino - Pequena - 3 furos Inox",
+//             "total_in_stock": 13,
+//             "availability_soldout": -1,
+//             "image_reference_sku_id": null,
+//             "updated_at": {
+//               "date": "2023-12-19 10:18:36.000000",
+//               "timezone": "America/Sao_Paulo",
+//               "timezone_type": 3
+//             },
+//             "variations": [
+//               {
+//                 "name": "Material",
+//                 "id": 2835904,
+//                 "value_id": 39959940,
+//                 "value": "Inox"
+//               }
+//             ],
+//             "product_id": 22194035,
+//             "allow_sell_without_customization": false,
+//             "id": 151463680,
+//             "sku": "116000003P",
+//             "barcode": null,
+//             "seller_id": null,
+//             "height": 4.7,
+//             "order": 0,
+//             "price_discount": 124.52,
+//             "erp_id": null,
+//             "length": 10.7,
+//             "purchase_url": "https://seguro.sertton-industrial.com.br/r/SVKICBZOT3",
+//             "blocked_sale": false,
+//             "weight": 0.5,
+//             "total_orders": null,
+//             "quantity_managed": true,
+//             "token": "SVKICBZOT3",
+//             "combinations": "39959940",
+//             "price_sale": 164.52,
+//             "width": 22.7,
+//             "customizations": {
+//               "data": []
+//             }
+//           }
+//         },
+//         "customizations": []
+//       }
+//     ]
+//   },
+//   "marketplace_account_id": null,
+//   "utm_source": null,
+//   "status": {
+//     "data": {
+//       "name": "Aguardando pagamento",
+//       "alias": "waiting_payment",
+//       "description": "Aguardando confirmação de pagamento",
+//       "id": 3
+//     }
+//   },
+//   "whatsapp": {
+//     "data": {
+//       "billet": {
+//         "link": "https://api.whatsapp.com/send?phone=5512988815499&text=Oi João. O seu pedido foi reservado com sucesso.%0a%0a*1 - Dobradiça com Mancal e pino - Pequena - 3 furos Inox*%0a%0aPara garantir os seus produtos, você precisa pagar o boleto o mais rápido possível, tá? Se preferir, você pode pagar usando o código de barras: %0a%0a*23791229286001337720991000046903295700000013629*",
+//         "message": "Oi João. O seu pedido foi reservado com sucesso.%0a%0a*1 - Dobradiça com Mancal e pino - Pequena - 3 furos Inox*%0a%0aPara garantir os seus produtos, você precisa pagar o boleto o mais rápido possível, tá? Se preferir, você pode pagar usando o código de barras: %0a%0a*23791229286001337720991000046903295700000013629*"
+//       },
+//       "order_shipped": null,
+//       "abandoned_cart": null,
+//       "pix": null
+//     }
+//   },
+//   "shipment_quote_id": "29",
+//   "payments": [
+//     {
+//       "icon_url": "https://icons.yampi.me/svg/billet.svg",
+//       "name": "Boleto Bancário",
+//       "alias": "billet"
+//     }
+//   ],
+//   "created_at": {
+//     "date": "2023-12-19 11:06:19.000000",
+//     "timezone": "America/Sao_Paulo",
+//     "timezone_type": 3
+//   },
+//   "merchant_id": 739383,
+//   "has_upsell": false,
+//   "buyer_value_total": 136.29,
+//   "days_delivery": 2,
+//   "reorder_url": "https://seguro.sertton-industrial.com.br/cart/items?customerToken=a53896e0-9e77-11ee-80f3-df06d65b1782&product_option_id[0]=151463680&quantity[0]=1&has_recomm[0]=0&redirectTo=checkout&skipToCheckout=1&store_token=HnPUUkOzxd4LwT5R5Jgw1fj7hlpMEfmDG3iOqmKK",
+//   "updated_at": {
+//     "date": "2023-12-20 06:40:47.000000",
+//     "timezone": "America/Sao_Paulo",
+//     "timezone_type": 3
+//   },
+//   "billet_whatsapp_link": "https://api.whatsapp.com/send?phone=5512988815499&text=Aqui está o boleto do produto *Dobradiça com Mancal e pino - Pequena - 3 furos Inox*, no valor de R$ 136,29%0a%0aVencimento: *20/12/2023*%0a%0aCódigo de barras: *23791229286001337720991000046903295700000013629*%0a%0aLink: https%3A%2F%2Fapi.pagar.me%2F1%2Fboletos%2Flive_clqcf4ctg5atb01m57ejewat6%3Fformat%3Dpdf",
+//   "value_shipment": 11.77,
+//   "spreadsheet": {
+//     "data": [
+//       {
+//         "total_item": 124.52,
+//         "total_cost": 62.26,
+//         "shipping_state": "Sao Paulo",
+//         "customization": "",
+//         "shipping_city": "Sao Jose dos Campos",
+//         "delivered": 0,
+//         "billet_barcode": "23791.22928 60013.377209 91000.046903 2 95700000013629",
+//         "customer_document": "44606238895",
+//         "shipping_neighborhood": "Vila Sao Bento",
+//         "payment": "Boleto bancário",
+//         "shipping_address": "Rua Aguapei, 284 - Vila Sao Bento",
+//         "sku": "116000003P",
+//         "payment_date": "",
+//         "shipping_street": "Rua Aguapei",
+//         "product": "Dobradiça com Mancal e pino - Pequena - 3 furos Inox",
+//         "quantity": 1,
+//         "customer_phone": "12988815499",
+//         "shipping_number": "284",
+//         "gateway_transaction_id": "ch_NKP1bVafGf2ZG6Qy",
+//         "credit_card": null,
+//         "cancelled_date": "",
+//         "customer_email": "rosangelaborgesbacha@hotmail.com",
+//         "shipping_complement": "",
+//         "customer": "Joao Pedro Carvalho dos Santos",
+//         "status": "Aguardando pagamento",
+//         "shipping_zip_code": "12231440"
+//       }
+//     ]
+//   },
+//   "shipping_address": {
+//     "data": {
+//       "country": "BR",
+//       "receiver": "João Pedro Carvalho dos Santos",
+//       "city": "São José dos Campos",
+//       "address_name": null,
+//       "full_address": "Rua Aguapei, 284 - Vila São Bento",
+//       "zip_code": "12231440",
+//       "zipcode": "12231440",
+//       "reference": null,
+//       "number": "284",
+//       "uf": "SP",
+//       "street": "Rua Aguapei",
+//       "id": 98812867,
+//       "neighborhood": "Vila São Bento",
+//       "state": "SP",
+//       "complement": null,
+//       "order_id": 102855369
+//     }
+//   },
+//   "shipment_icon_url": "https://icons.yampi.me/svg/correios.svg",
+//   "utm_content": null,
+//   "whatsapp_app": {
+//     "data": {
+//       "billet": {
+//         "link": "whatsapp://send?phone=5512988815499&text=Oi João. O seu pedido foi reservado com sucesso.%0a%0a*1 - Dobradiça com Mancal e pino - Pequena - 3 furos Inox*%0a%0aPara garantir os seus produtos, você precisa pagar o boleto o mais rápido possível, tá? Se preferir, você pode pagar usando o código de barras: %0a%0a*23791229286001337720991000046903295700000013629*",
+//         "message": "Oi João. O seu pedido foi reservado com sucesso.%0a%0a*1 - Dobradiça com Mancal e pino - Pequena - 3 furos Inox*%0a%0aPara garantir os seus produtos, você precisa pagar o boleto o mais rápido possível, tá? Se preferir, você pode pagar usando o código de barras: %0a%0a*23791229286001337720991000046903295700000013629*"
+//       },
+//       "order_shipped": null,
+//       "abandoned_cart": null,
+//       "pix": null
+//     }
+//   },
+//   "total_comments": 0,
+//   "desire_status": [
+//     "cancelled",
+//     "paid",
+//     "refused"
+//   ],
+//   "shipment_cost": 11.77,
+//   "services": {
+//     "data": []
+//   },
+//   "promocode_id": null,
+//   "sync_by_erp": false,
+//   "utm_term": null,
+//   "date_delivery": {
+//     "date": "2023-12-21 00:00:00.000000",
+//     "timezone": "America/Sao_Paulo",
+//     "timezone_type": 3
+//   },
+//   "value_total": 136.29,
+//   "track_code": null,
+//   "b2w_label_url": null,
+//   "customer_id": 179128559,
+//   "track_url": null,
+//   "customer": {
+//     "data": {
+//       "marketplace_id": null,
+//       "birthday": null,
+//       "whatsapp": false,
+//       "newsletter": false,
+//       "notes": null,
+//       "anonymized": false,
+//       "created_at": {
+//         "date": "2023-12-19 11:05:20.000000",
+//         "timezone": "America/Sao_Paulo",
+//         "timezone_type": 3
+//       },
+//       "merchant_id": 739383,
+//       "cnpj": null,
+//       "type": "f",
+//       "cluster_id": null,
+//       "login_url": "https://seguro.sertton-industrial.com.br/auth/login/force?token=a53896e0-9e77-11ee-80f3-df06d65b1782",
+//       "updated_at": {
+//         "date": "2023-12-19 11:05:20.000000",
+//         "timezone": "America/Sao_Paulo",
+//         "timezone_type": 3
+//       },
+//       "social_driver": null,
+//       "razao_social": null,
+//       "cpf": "44606238895",
+//       "id": 179128559,
+//       "first_name": "João",
+//       "email": "rosangelaborgesbacha@hotmail.com",
+//       "utm_campaign": null,
+//       "generic_name": "João Pedro Carvalho dos Santos",
+//       "ip": "187.95.31.48",
+//       "active": true,
+//       "last_name": "Santos",
+//       "token": "a53896e0-9e77-11ee-80f3-df06d65b1782",
+//       "state_registration": null,
+//       "social_id": null,
+//       "phone": {
+//         "number": "988815499",
+//         "formated_number": "(12) 98881-5499",
+//         "area_code": "12",
+//         "full_number": "12988815499",
+//         "whatsapp_link": "https://api.whatsapp.com/send?phone=5512988815499"
+//       },
+//       "name": "João Pedro Carvalho dos Santos",
+//       "utm_source": null
+//     }
+//   }
+// }
