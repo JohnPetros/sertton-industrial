2b53cd76dd470e95cb1f5dcf2519bee5a00b62af
João Pedro <joaopedro.nc@outlook.com>
2023-12-23 11:46:14 -0300

    🏷️ type: add isNumeric prop to List component

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -4,29 +4,39 @@ import { getTokens, ListItem, Text, YGroup } from 'tamagui'
 interface ListProps {
   items: string[]
   bgColor?: '$gray50' | '$white'
+  isNumeric?: boolean
 }
 
-export function List({ items, bgColor = '$gray50' }: ListProps) {
+export function List({
+  items,
+  bgColor = '$gray50',
+  isNumeric = false,
+}: ListProps) {
   return (
     <YGroup>
-      {items.map((item) => (
+      {items.map((item, index) => (
         <YGroup.Item key={item}>
           <ListItem
             my={-4}
             px={0}
             fontSize={12}
+            gap={8}
             alignItems="center"
             justifyContent="flex-start"
             bg={bgColor}
-            icon={
+          >
+            {isNumeric ? (
+              <Text color="$gray800">{index + 1}.</Text>
+            ) : (
               <Circle
                 size={8}
-                color={getTokens().color.green800.val}
+                color={getTokens().color.gray800.val}
                 weight="fill"
               />
-            }
-          >
-            <Text textAlign="left">{item}</Text>
+            )}
+            <Text textAlign="left" flexWrap="wrap" flex={1}>
+              {item}
+            </Text>
           </ListItem>
         </YGroup.Item>
       ))}
