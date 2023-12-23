15f794c118f5b1255d0b9bf3a2ac118c96000ac9
João Pedro <joaopedro.nc@outlook.com>
2023-12-23 13:16:24 -0300

    ✨ feat (ui): add InformativeHighlights component

---
+++ src/components/InformativeHighlights/index.tsx
@@ -0,0 +1,80 @@
+import { SwiperFlatList } from 'react-native-swiper-flatlist'
+import { ArrowLeft, ArrowRight } from 'phosphor-react-native'
+import { getTokens, Text, XStack } from 'tamagui'
+
+import { Button } from '@/components/Button'
+import { useInformativeHighlights } from '@/components/InformativeHighlights/useInformativeHighlights'
+import { INFORMATIVE_HIGHLIGHTS } from '@/utils/constants/InformativeHighlights'
+import { SCREEN } from '@/utils/constants/screen'
+
+export function InformativeHighlights() {
+  const { swiperRef, handlePrev, handleNext } = useInformativeHighlights(
+    INFORMATIVE_HIGHLIGHTS.length - 1
+  )
+
+  return (
+    <XStack>
+      <Button
+        position="absolute"
+        top={-12}
+        left={-24}
+        zIndex={50}
+        background="transparent"
+        onPress={handlePrev}
+        icon={
+          <ArrowLeft
+            size={16}
+            weight="bold"
+            color={getTokens().color.gray800.val}
+          />
+        }
+      />
+      <SwiperFlatList
+        ref={swiperRef}
+        autoplay
+        autoplayDelay={2}
+        autoplayLoop
+        disableGesture
+        autoplayInvertDirection
+        data={INFORMATIVE_HIGHLIGHTS}
+        renderItem={({ item }) => {
+          const Icon = item.icon
+          return (
+            <XStack
+              key={item.text}
+              w={SCREEN.width}
+              alignItems="center"
+              justifyContent="center"
+              gap={8}
+              x={-24}
+            >
+              <Icon
+                color={getTokens().color.blue500.val}
+                size={24}
+                weight="bold"
+              />
+              <Text color="$gray800" fontWeight="600" fontSize={14}>
+                {item.text}
+              </Text>
+            </XStack>
+          )
+        }}
+      />
+      <Button
+        position="absolute"
+        top={-12}
+        right={-24}
+        zIndex={50}
+        background="transparent"
+        onPress={handleNext}
+        icon={
+          <ArrowRight
+            size={16}
+            weight="bold"
+            color={getTokens().color.gray800.val}
+          />
+        }
+      />
+    </XStack>
+  )
+}
