93c3993e99c5911e26e2d5d243a0ee64c59e9e4c
João Pedro <joaopedro.nc@outlook.com>
2023-12-23 11:50:31 -0300

    ♻️ refactor: add BackButton to Profile screen

--- src/app/(stack)/profile.tsx
+++ src/app/(stack)/profile.tsx
@@ -1,30 +1,34 @@
 import { useRouter } from 'expo-router/src/hooks'
-import { ArrowLeft } from 'phosphor-react-native'
+import { ArrowLeft, User } from 'phosphor-react-native'
 import { getTokens, H1, YStack } from 'tamagui'
 
+import { BackButton } from '@/components/BackButton'
 import { Button } from '@/components/Button'
+import { EmptyItemsMessage } from '@/components/EmptyItemsMessage'
 import { Profile } from '@/components/Profile'
+import { useCustomerContext } from '@/contexts/CustomerContext'
 import { SCREEN } from '@/utils/constants/screen'
 
 export default function ProfileScreen() {
   const router = useRouter()
+  const { customer } = useCustomerContext()
 
   return (
-    <YStack px={SCREEN.paddingX}>
-      <Button
-        background="transparent"
-        alignSelf="flex-start"
-        fontSize={16}
-        onPress={router.back}
-        px={0}
-      >
-        <ArrowLeft color={getTokens().color.gray800.val} />
-        Voltar
-      </Button>
+    <YStack px={SCREEN.paddingX} flex={1}>
+      <BackButton />
       <H1 fontSize={24} color="$gray800">
         Meu cadastro
       </H1>
-      <Profile />
+      {!customer && (
+        <EmptyItemsMessage
+          title="Cadastro não encontrado"
+          icon={User}
+          callback={
+            <Button onPress={router.back}>Voltar para o aplicativo</Button>
+          }
+        />
+      )}
+      {customer && <Profile />}
     </YStack>
   )
 }
