ce19a32a817346aeb54727c828a1f76ceba5fd3d
João Pedro <joaopedro.nc@outlook.com>
2023-12-23 13:17:05 -0300

    ✨ feat (ui): add CreditCardTypes component

--- src/app/(drawer)/(tabs)/home.tsx
+++ src/app/(drawer)/(tabs)/home.tsx
@@ -1,35 +1,51 @@
-import { YStack } from 'tamagui'
+import { ScrollView, YStack } from 'tamagui'
 
 import { Collection } from '@/components/Collection'
+import { Footer } from '@/components/Footer'
 import { Search } from '@/components/Form/Search'
 import { Header } from '@/components/Header'
+import { InformativeHighlights } from '@/components/InformativeHighlights'
+import { LeadCapture } from '@/components/LeadCapture'
 import { useCollections } from '@/hooks/useCollections'
 import { collectionsMock } from '@/tests/mocks/collectionsMock'
+import { SCREEN } from '@/utils/constants/screen'
 
 export default function Home() {
   const { collections, isLoading } = useCollections()
 
   return (
-    <YStack px={24}>
-      <Header />
-      <Search />
-      <YStack mt={24}>
-        {isLoading ? (
-          <Collection
-            key={collectionsMock[0].id}
-            data={collectionsMock[0]}
-            isLoading={true}
-          />
-        ) : (
-          collections?.map((collection) => (
-            <Collection
-              key={collection.id}
-              data={collection}
-              isLoading={false}
-            />
-          ))
-        )}
+    <YStack>
+      <YStack px={SCREEN.paddingX}>
+        <Header />
+        <Search />
       </YStack>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: SCREEN.paddingBottom }}
+      >
+        <YStack mt={24} gap={24} px={SCREEN.paddingX}>
+          <InformativeHighlights />
+          {isLoading ? (
+            <Collection
+              key={collectionsMock[0].id}
+              data={collectionsMock[0]}
+              isLoading={true}
+            />
+          ) : (
+            collections?.map((collection) => (
+              <Collection
+                key={collection.id}
+                data={collection}
+                isLoading={false}
+              />
+            ))
+          )}
+        </YStack>
+        <YStack mt={40}>
+          <LeadCapture />
+        </YStack>
+        <Footer />
+      </ScrollView>
     </YStack>
   )
 }
--- src/components/Checkout/PaymentForm/CreditCardForm/CreditCardTypes/index.tsx
+++ src/components/Checkout/PaymentForm/CreditCardForm/CreditCardTypes/index.tsx
@@ -1,5 +1,4 @@
 import { SvgUri } from 'react-native-svg'
-import { XStack } from 'tamagui'
 
 import { useCreditCardTypes } from '@/components/Checkout/PaymentForm/CreditCardForm/CreditCardTypes/useCreditCardTypes'
 
@@ -7,11 +6,7 @@ export function CreditCardTypes() {
   const { creditCardTypes } = useCreditCardTypes()
 
   if (creditCardTypes)
-    return (
-      <XStack gap={8} flexWrap="wrap" width="90%">
-        {creditCardTypes.map((creditCardType) => (
-          <SvgUri key={creditCardType.name} uri={creditCardType.icon} />
-        ))}
-      </XStack>
-    )
+    return creditCardTypes.map((creditCardType) => (
+      <SvgUri key={creditCardType.name} uri={creditCardType.icon} />
+    ))
 }
