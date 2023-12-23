c0f51034668dbf91ed76d73d22792f42e0c6f203
João Pedro <joaopedro.nc@outlook.com>
2023-12-23 13:15:44 -0300

    ✨ feat (utils): add  INFORMATIVE_HIGHLIGHTS constant

---
+++ src/utils/constants/InformativeHighlights.ts
@@ -0,0 +1,26 @@
+import {
+  ChatTeardropText,
+  CreditCard,
+  Icon,
+  LockSimple,
+  Truck,
+} from 'phosphor-react-native'
+
+export const INFORMATIVE_HIGHLIGHTS: { text: string; icon: Icon }[] = [
+  {
+    text: 'Envio rápido e garantido, para todo Brasil',
+    icon: Truck,
+  },
+  {
+    text: 'Parcele suas compras com cartão de crédito',
+    icon: CreditCard,
+  },
+  {
+    text: 'Loja confiável com tecnologia 100% segura',
+    icon: LockSimple,
+  },
+  {
+    text: 'Suporte rápido e prestativo',
+    icon: ChatTeardropText,
+  },
+]
