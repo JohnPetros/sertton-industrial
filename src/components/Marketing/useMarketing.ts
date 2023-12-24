29b86c143a22678d02c21f2344816bed5fcbc73b
João Pedro <joaopedro.nc@outlook.com>
2023-12-24 14:04:22 -0300

    ✨ feat (hooks): add useMarketing hook

---
+++ src/components/Marketing/useMarketing.ts
@@ -0,0 +1,32 @@
+import { useMemo } from 'react'
+
+import { Banner } from '@/@types/banner'
+import { Collection } from '@/@types/collection'
+
+export type MarketingItemType = 'banner' | 'collection'
+
+export function useMarketing(banners: Banner[], collections: Collection[]) {
+  const items = useMemo(() => {
+    const items = []
+    const limit = Math.min(banners.length, collections.length)
+    for (let index = 0; index < limit; index++) {
+      items.push({ type: 'banner', data: banners[index] })
+      items.push({ type: 'collection', data: collections[index] })
+    }
+    return items
+  }, [banners, collections])
+
+  function isCollection(data: any): data is Collection {
+    return 'products' in data
+  }
+
+  function isBanner(data: any): data is Banner {
+    return 'mobile_image_url' in data
+  }
+
+  return {
+    items,
+    isCollection,
+    isBanner,
+  }
+}
