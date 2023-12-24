028a40978ff1ce365254c05df5af0c0a417de146
João Pedro <joaopedro.nc@outlook.com>
2023-12-24 13:55:54 -0300

    ✨ feat (hooks): add useBanners hook

---
+++ src/components/Marketing/useBanners.ts
@@ -0,0 +1,20 @@
+import { useQuery } from 'react-query'
+
+import { useAppError } from '@/components/AppError/useAppError'
+import { useApi } from '@/services/api'
+
+export function useBanners() {
+  const api = useApi()
+  const { throwAppError } = useAppError()
+
+  const { data } = useQuery('banners', () => api.getBanners(), {
+    onError: (error) => {
+      api.handleError(error)
+      throwAppError('Erro ao mostrar banners')
+    },
+  })
+
+  return {
+    banners: data,
+  }
+}
