00fcde94020c132d92ccdc44c936fee215a9fc06
João Pedro <joaopedro.nc@outlook.com>
2023-12-24 13:57:40 -0300

    ✨ feat (ui): add Collection component

---
+++ src/components/Marketing/index.tsx
@@ -0,0 +1,38 @@
+import { YStack } from 'tamagui'
+
+import { Banner } from '@/components/Marketing/Banner'
+import { Collection } from '@/components/Marketing/Collection'
+import { useBanners } from '@/components/Marketing/useBanners'
+import { useCollections } from '@/components/Marketing/useCollections'
+import { useMarketing } from '@/components/Marketing/useMarketing'
+import { collectionsMock } from '@/tests/mocks/collectionsMock'
+
+export function Marketing() {
+  const { banners } = useBanners()
+  const { collections, isLoading } = useCollections()
+  const { items, isBanner, isCollection } = useMarketing(
+    banners ?? [],
+    collections ?? []
+  )
+
+  return isLoading ? (
+    <Collection
+      key={collectionsMock[0].id}
+      data={collectionsMock[0]}
+      isLoading={true}
+    />
+  ) : (
+    items?.map((item) => {
+      if (item.type === 'banner' && isBanner(item.data)) {
+        return <Banner key={item.data.id} data={item.data} />
+      }
+      if (item.type === 'collection' && isCollection(item.data)) {
+        return (
+          <YStack key={item.data.id} px={24} py={24}>
+            <Collection data={item.data} isLoading={false} />
+          </YStack>
+        )
+      }
+    })
+  )
+}
