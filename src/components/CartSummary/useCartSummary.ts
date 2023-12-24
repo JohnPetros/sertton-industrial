b2875421ba145ed9bfe4230639543a275485ed54
João Pedro <joaopedro.nc@outlook.com>
2023-12-24 14:02:50 -0300

    🐛 fix: ensure calculate discount on purchases above certain values

--- src/components/CartSummary/useCartSummary.ts
+++ src/components/CartSummary/useCartSummary.ts
@@ -1,19 +1,32 @@
 import { useEffect, useState } from 'react'
+import { useQuery } from 'react-query'
 
 import { Product as ProductData } from '@/@types/product'
+import { useAppError } from '@/components/AppError/useAppError'
+import { useApi } from '@/services/api'
 import { useCartStore } from '@/stores/cartStore'
 
 type Product = ProductData & { quantity: number; selectedSkuId: number }
 
 export function useCartSummary(products: Product[]) {
   const cartItems = useCartStore((store) => store.state.items)
+  const api = useApi()
+  const { throwAppError } = useAppError()
 
+  const { data: discounts } = useQuery('discounts', () => api.getDiscounts(), {
+    onError: (error) => {
+      api.handleError(error)
+      throwAppError('Erro ao calcular desconto de compra')
+    },
+  })
+
+  const [subtotal, setSubtotal] = useState(0)
   const [totalToPay, setTotalToPay] = useState(0)
   const [totalDiscount, setTotalDiscount] = useState(0)
   const [totalItems, setTotalItems] = useState(0)
 
   function calculateTotals() {
-    const totalToPay = products.reduce((total, product) => {
+    const totalProductsToPay = products.reduce((total, product) => {
       const selectedSku = product.skus.data.find(
         (sku) => sku.id === product.selectedSkuId
       )
@@ -30,7 +43,7 @@ export function useCartSummary(products: Product[]) {
       else return total
     }, 0)
 
-    const totalDiscount = products.reduce((total, product) => {
+    const totalProductsDiscount = products.reduce((total, product) => {
       const selectedSku = product.skus.data.find(
         (sku) => sku.id === product.selectedSkuId
       )
@@ -55,8 +68,20 @@ export function useCartSummary(products: Product[]) {
       return quantity ? total + quantity : 0
     }, 0)
 
-    setTotalToPay(totalToPay)
+    const totalToPay = totalProductsToPay - totalProductsDiscount
+
+    let totalDiscount = totalProductsDiscount
+
+    if (discounts) {
+      for (const discount of discounts) {
+        if (totalToPay - totalProductsDiscount >= discount.min_value)
+          totalDiscount = totalDiscount + totalToPay * (discount.percent / 100)
+      }
+    }
+
     setTotalDiscount(totalDiscount)
+    setSubtotal(totalProductsToPay)
+    setTotalToPay(totalProductsToPay - totalDiscount)
     setTotalItems(totalItems)
   }
 
@@ -65,6 +90,7 @@ export function useCartSummary(products: Product[]) {
   }, [cartItems])
 
   return {
+    subtotal,
     totalToPay,
     totalDiscount,
     totalItems,
