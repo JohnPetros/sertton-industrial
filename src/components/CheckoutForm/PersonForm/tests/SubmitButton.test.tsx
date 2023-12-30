c64b70bae84c8518d3b6593c3fa587a35c95d380
João Pedro <joaopedro.nc@outlook.com>
2023-12-30 17:42:34 -0300

    🧪 test: SubmitButton component

---
+++ src/components/CheckoutForm/PersonForm/tests/SubmitButton.test.tsx
@@ -0,0 +1,63 @@
+import { fireEvent, screen } from '@testing-library/react-native'
+import { usePathname } from 'expo-router'
+
+import { SubmitButton } from '@/components/CheckoutForm/PersonForm/SubmitButton'
+import { render } from '@/tests/render'
+
+jest.mock('expo-router')
+
+const submitMock = jest.fn()
+
+describe('SubmitButton component', () => {
+  it('should render spinner when is submiting', () => {
+    render(<SubmitButton handleSubmit={submitMock} isSubmitting={true} />)
+
+    const spinner = screen.getByTestId('spinner')
+
+    expect(spinner).toBeTruthy()
+  })
+
+  it('should call submit function on press', () => {
+    render(<SubmitButton handleSubmit={submitMock} isSubmitting={true} />)
+
+    const button = screen.getByTestId('submit-button')
+
+    fireEvent.press(button)
+
+    expect(submitMock).toHaveBeenCalled()
+  })
+
+  it('should call submit function on press', () => {
+    const usePathnameMock = jest.mocked(usePathname)
+
+    usePathnameMock.mockReturnValueOnce('/profile')
+
+    render(<SubmitButton handleSubmit={submitMock} isSubmitting={true} />)
+
+    const button = screen.getByTestId('submit-button')
+
+    fireEvent.press(button)
+
+    expect(submitMock).toHaveBeenCalled()
+  })
+
+  it('should render title "Continuar" on checkout screen', async () => {
+    const usePathnameMock = jest.mocked(usePathname)
+
+    usePathnameMock.mockReturnValueOnce('/checkout')
+
+    render(<SubmitButton handleSubmit={submitMock} isSubmitting={false} />)
+
+    expect(screen.getByText('Continuar')).toBeTruthy()
+  })
+
+  it('should render title "Atualizar cadastro" on profile screen', async () => {
+    const usePathnameMock = jest.mocked(usePathname)
+
+    usePathnameMock.mockReturnValueOnce('/profile')
+
+    render(<SubmitButton handleSubmit={submitMock} isSubmitting={false} />)
+
+    expect(screen.getByText('Atualizar cadastro')).toBeTruthy()
+  })
+})
