95de600c954478f6a799ba6330b480e24529e79b
João Pedro <joaopedro.nc@outlook.com>
2023-12-30 17:44:28 -0300

    🧪 test: add render util

--- src/tests/render.tsx
+++ src/tests/render.tsx
@@ -4,5 +4,5 @@ import { render as renderComponent } from '@testing-library/react-native'
 import { TamaguiProvider } from '@/providers/TamaguiProvider'
 
 export function render(component: ReactNode) {
-  renderComponent(<TamaguiProvider>{component}</TamaguiProvider>)
+  return renderComponent(<TamaguiProvider>{component}</TamaguiProvider>)
 }
