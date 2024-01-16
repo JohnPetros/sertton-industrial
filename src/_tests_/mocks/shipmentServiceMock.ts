e68c5770fba5d0cb55de65173ca0b932d405891b
João Pedro <joaopedro.nc@outlook.com>
2024-01-16 15:25:51 -0300

    🧪 test: add shipmentServicesMock

---
+++ src/_tests_/mocks/shipmentServiceMock.ts
@@ -0,0 +1,20 @@
+export const shipmentServicesMock = [
+  {
+    name: 'DHL',
+    service: 'Express',
+    price: 15.99,
+    days: 2,
+  },
+  {
+    name: 'UPS',
+    service: 'Standard',
+    price: 10.99,
+    days: 3,
+  },
+  {
+    name: 'FedEx',
+    service: 'Overnight',
+    price: 25.99,
+    days: 1,
+  },
+]
