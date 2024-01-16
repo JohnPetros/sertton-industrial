6feb72368df9b446a53c0b1faaf8be26716be971
João Pedro <joaopedro.nc@outlook.com>
2024-01-16 20:19:42 -0300

    ✨ feat (validation): add  number regex

--- src/services/validation/zod/schemas/creditCardExpirationDateSchema.ts
+++ src/services/validation/zod/schemas/creditCardExpirationDateSchema.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod'
 
 import { VALIDATION_ERRORS } from '@/services/validation/config/validationErrors'
+import { REGEX } from '@/utils/constants/regex'
 
 export const creditCardExpirationDateSchema = z
   .string({
     required_error: VALIDATION_ERRORS.required,
   })
   .length(4, VALIDATION_ERRORS.creditCardExpirationDate.length)
+  .regex(REGEX.number, VALIDATION_ERRORS.number)
--- src/services/validation/zod/schemas/phoneSchema.ts
+++ src/services/validation/zod/schemas/phoneSchema.ts
@@ -2,8 +2,11 @@ import { z } from 'zod'
 
 import { VALIDATION_ERRORS } from '../../config/validationErrors'
 
+import { REGEX } from '@/utils/constants/regex'
+
 export const phoneSchema = z
   .string({
     required_error: VALIDATION_ERRORS.required,
   })
   .length(11, VALIDATION_ERRORS.phone.length)
+  .regex(REGEX.number, VALIDATION_ERRORS.number)
