ede4b6cfe52ea57224e7b4c39534b9e208fc478c
João Pedro <joaopedro.nc@outlook.com>
2024-01-16 12:55:09 -0300

    🧪 test: add apiAddressMockResponse mock

---
+++ src/_tests_/mocks/apiAddressMockResponse.ts
@@ -0,0 +1,8 @@
+export const apiAddressMockResponse = {
+  uf: 'PT',
+  localidade: 'Cidade dos Poetas Mortos',
+  logradouro: 'Rua dos bocós',
+  bairro: 'Bairro de Deus',
+  cep: '98745612',
+  erro: null,
+}
