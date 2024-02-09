b140afc21bb9a91b288cac3858f7fe7c00efc944
João Pedro <joaopedro.nc@outlook.com>
2024-02-09 00:17:40 -0300

    🏷️ type: correct Customer type

--- src/@types/customer.ts
+++ src/@types/customer.ts
@@ -1,20 +1,17 @@
 import type { Address } from '@/@types/address'
 
+export type PersonType = 'natural' | 'legal'
+
 export type Customer = {
   id: number
-  type: 'f' | 'j'
+  personType: PersonType
+  email: string
   name?: string
   cpf?: string
   cnpj?: string
-  email: string
-  razao_social?: string
+  razaoSocial?: string
   active: boolean
-  homephone: string
-  addresses: {
-    data: Address[]
-  }
-  phone?: {
-    full_number: string
-  }
+  addresses: Address[]
+  phone: string | null
   selectedAddressZipcode?: string | null
 }
