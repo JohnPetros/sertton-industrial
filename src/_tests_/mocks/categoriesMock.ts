009cb57151a9b220a4513b31dde46494977e223a
João Pedro <joaopedro.nc@outlook.com>
2024-02-11 14:37:11 -0300

    🧪 test: add categoriesMock

---
+++ src/_tests_/mocks/categoriesMock.ts
@@ -0,0 +1,29 @@
+import type { Category } from '@/@types/category'
+
+export const categoriesMock: Category[] = [
+  {
+    id: '1',
+    name: 'First Category',
+    description: 'Description for the first category.',
+  },
+  {
+    id: '2',
+    name: 'Second Category',
+    description: 'Description for the second category.',
+  },
+  {
+    id: '3',
+    name: 'Third Category',
+    description: 'Description for the third category.',
+  },
+  {
+    id: '4',
+    name: 'Fourth Category',
+    description: 'Description for the forth category.',
+  },
+  {
+    id: '5',
+    name: 'Fifth Category',
+    description: 'Description for the Fifth category.',
+  },
+]
